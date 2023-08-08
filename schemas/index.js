8437ebde71ccaa27e9e0916a961579e014f846fd
hyojinkim2028 <134348257+hyojinkim2028@users.noreply.github.com>
2023-08-08 21:51:30 +0900

    Update index.js

--- schemas/index.js
+++ schemas/index.js
@@ -5,8 +5,8 @@ const connect = () => {
     mongoose.set("debug", true);
   }
   mongoose
-    .connect("mongodb://127.0.0.1:27017/prac", {
-      dbName: "prac",
+    .connect("mongodb://127.0.0.1:27017/comments_db", {
+      dbName: "comments_db",
       useNewUrlParser: true,
     })
     .then(() => {
